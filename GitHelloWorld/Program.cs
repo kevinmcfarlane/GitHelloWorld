eb147abf43fda7bc4c2de836ce49073199d9e9dc
Kevin McFarlane <kevin.mcfarlane@gmail.com>
2016-01-27 10:17:30 +0000

    Initial commit.

---
+++ GitHelloWorld/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GitHelloWorld
+{
+    class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello");
+        }
+    }
+}
