5c68a049cb91bce620c5d41b1f73c5b4f991365d
Kevin McFarlane <kevin.mcfarlane@gmail.com>
2016-01-27 16:20:01 +0000

    Edited string.

--- GitHelloWorld/Program.cs
+++ GitHelloWorld/Program.cs
@@ -6,7 +6,7 @@ namespace GitHelloWorld
     {
         private static void Main(string[] args)
         {
-            Console.WriteLine("Hello");
+            Console.WriteLine("Hello World");
         }
     }
 }
